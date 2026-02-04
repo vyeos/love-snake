f4a7953f5f366c8119f98b1e3539ced13a8f0260
vyeos <rudranpatel0980@gmail.com>
2026-02-04 17:00:06 +0530

    snake wraps around edges

--- main.lua
+++ main.lua
@@ -5,24 +5,44 @@ function love.load()
 		{ x = 1, y = 1 },
 	}
 	Timer = 0
-	Direction = "right"
+	DirectionQueue = { "right" }
+
+	GridXCount = 20 -- cells
+	GridYCount = 15 -- cells
 end
 
 function love.update(dt)
 	Timer = Timer + dt
 	if Timer >= 0.15 then
 		Timer = 0
+
+		if #DirectionQueue > 1 then
+			table.remove(DirectionQueue, 1)
+		end
+
 		local nextXPosition = SnakeSegments[1].x
 		local nextYPosition = SnakeSegments[1].y
 
-		if Direction == "right" then
+		if DirectionQueue[1] == "right" then
 			nextXPosition = nextXPosition + 1
-		elseif Direction == "left" then
+			if nextXPosition > GridXCount then
+				nextXPosition = 1
+			end
+		elseif DirectionQueue[1] == "left" then
 			nextXPosition = nextXPosition - 1
-		elseif Direction == "down" then
+			if nextXPosition < 1 then
+				nextXPosition = GridXCount
+			end
+		elseif DirectionQueue[1] == "down" then
 			nextYPosition = nextYPosition + 1
-		elseif Direction == "up" then
+			if nextYPosition > GridYCount then
+				nextYPosition = 1
+			end
+		elseif DirectionQueue[1] == "up" then
 			nextYPosition = nextYPosition - 1
+			if nextYPosition < 1 then
+				nextYPosition = GridYCount
+			end
 		end
 
 		table.insert(SnakeSegments, 1, {
@@ -34,24 +54,26 @@ function love.update(dt)
 end
 
 function love.keypressed(key)
-	if key == "right" and Direction ~= "left" then
-		Direction = "right"
-	elseif key == "left" and Direction ~= "right" then
-		Direction = "left"
-	elseif key == "up" and Direction ~= "down" then
-		Direction = "up"
-	elseif key == "down" and Direction ~= "up" then
-		Direction = "down"
+	if key == "right" and DirectionQueue[#DirectionQueue] ~= "right" and DirectionQueue[#DirectionQueue] ~= "left" then
+		table.insert(DirectionQueue, "right")
+	elseif
+		key == "left"
+		and DirectionQueue[#DirectionQueue] ~= "left"
+		and DirectionQueue[#DirectionQueue] ~= "right"
+	then
+		table.insert(DirectionQueue, "left")
+	elseif key == "up" and DirectionQueue[#DirectionQueue] ~= "up" and DirectionQueue[#DirectionQueue] ~= "down" then
+		table.insert(DirectionQueue, "up")
+	elseif key == "down" and DirectionQueue[#DirectionQueue] ~= "down" and DirectionQueue[#DirectionQueue] ~= "up" then
+		table.insert(DirectionQueue, "down")
 	end
 end
 
 function love.draw()
-	local gridXCount = 20 -- cells
-	local gridYCount = 15 -- cells
 	local cellSize = 15 -- pixels
 
 	love.graphics.setColor(0.28, 0.28, 0.28)
-	love.graphics.rectangle("fill", 0, 0, gridXCount * cellSize, gridYCount * cellSize)
+	love.graphics.rectangle("fill", 0, 0, GridXCount * cellSize, GridYCount * cellSize)
 
 	for _, segment in ipairs(SnakeSegments) do
 		love.graphics.setColor(0.6, 1, 0.32)
@@ -62,5 +84,10 @@ function love.draw()
 			cellSize - 1,
 			cellSize - 1
 		)
+	end
+
+	for directionIndex, direction in ipairs(DirectionQueue) do
+		love.graphics.setColor(1, 1, 1)
+		love.graphics.print("DirectionQueue[" .. directionIndex .. "]: " .. direction, 15 * 15 * directionIndex)
 	end
 end
