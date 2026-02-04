903f1d8123496315eab842aede8a5d3af73aa3a2
vyeos <rudranpatel0980@gmail.com>
2026-02-04 12:21:35 +0530

    snake can move with keys

--- main.lua
+++ main.lua
@@ -1,3 +1,50 @@
+function love.load()
+	SnakeSegments = {
+		{ x = 3, y = 1 },
+		{ x = 2, y = 1 },
+		{ x = 1, y = 1 },
+	}
+	Timer = 0
+	Direction = "right"
+end
+
+function love.update(dt)
+	Timer = Timer + dt
+	if Timer >= 0.15 then
+		Timer = 0
+		local nextXPosition = SnakeSegments[1].x
+		local nextYPosition = SnakeSegments[1].y
+
+		if Direction == "right" then
+			nextXPosition = nextXPosition + 1
+		elseif Direction == "left" then
+			nextXPosition = nextXPosition - 1
+		elseif Direction == "down" then
+			nextYPosition = nextYPosition + 1
+		elseif Direction == "up" then
+			nextYPosition = nextYPosition - 1
+		end
+
+		table.insert(SnakeSegments, 1, {
+			x = nextXPosition,
+			y = nextYPosition,
+		})
+		table.remove(SnakeSegments)
+	end
+end
+
+function love.keypressed(key)
+	if key == "right" and Direction ~= "left" then
+		Direction = "right"
+	elseif key == "left" and Direction ~= "right" then
+		Direction = "left"
+	elseif key == "up" and Direction ~= "down" then
+		Direction = "up"
+	elseif key == "down" and Direction ~= "up" then
+		Direction = "down"
+	end
+end
+
 function love.draw()
 	local gridXCount = 20 -- cells
 	local gridYCount = 15 -- cells
@@ -6,13 +53,7 @@ function love.draw()
 	love.graphics.setColor(0.28, 0.28, 0.28)
 	love.graphics.rectangle("fill", 0, 0, gridXCount * cellSize, gridYCount * cellSize)
 
-	local snakeSegments = {
-		{ x = 3, y = 1 },
-		{ x = 2, y = 1 },
-		{ x = 1, y = 1 },
-	}
-
-	for _, segment in ipairs(snakeSegments) do
+	for _, segment in ipairs(SnakeSegments) do
 		love.graphics.setColor(0.6, 1, 0.32)
 		love.graphics.rectangle(
 			"fill",
